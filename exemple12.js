c56357bfa7b83ea7c371e4a319c651bb434dfa71
seckene <seckene_sako@hotmail.com>
2024-10-17 22:41:00 +0200

    Add files via upload

---
+++ exemple12.js
@@ -0,0 +1,13 @@
+
+
+let sex = prompt("Êtes-vous un monsieur ou une dame  ")
+
+
+
+
+let non =prompt("quelle est votre NOM");
+
+
+alert("selectionner l'une des cuisisne dans la liste suivante ")
+let popo=prompt("quelle type de cuisine vous interesse ? : Contemporaine ,Traditionnelle ,Cuisine de Luxe")
+alert( sex +" " + non + " voici l'une des liste qui pourait vous correspond"+ "")
